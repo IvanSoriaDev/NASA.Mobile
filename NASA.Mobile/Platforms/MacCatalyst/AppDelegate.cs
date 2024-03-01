34a30fbdd0c43e8011fdb4c16ece61f46c2a52f9
Ivan Soria <jisorial@webcreek.com>
2024-03-01 12:35:36 -0600

    Add project files.

---
+++ NASA.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NASA.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
